7ab81b833a4ebb2024e82d2a39cd8f2ed0fbea3b
Nurzhan <252058@astanait.edu.kz>
2025-12-22 16:16:45 +0500

    Commit

---
+++ src/com/Nurzhan/grocerystore/Main.java
@@ -0,0 +1,11 @@
+package com.Nurzhan.grocerystore;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to my Grocery Store System!");
+        System.out.println("This program will manage products, customers and sales.");
+        System.out.println("Feature: add and remove products.");
+        System.out.println("Feature: view customer purchase history.");
+        System.out.println("Feature: calculate receipts.");
+    }
+}
